289760327855b6315fcecc527617c843b6815676
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-07-08 08:35:45 +0300

    initial commit

---
+++ lib/my_home_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final border = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(
+        (16),
+      ),
+      borderSide: const BorderSide(
+        color: Colors.black,
+        width: 2,
+        strokeAlign: BorderSide.strokeAlignOutside,
+        style: BorderStyle.solid,
+      ),
+    );
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: Center(
+        child: Column(
+          children: [
+            const Text(
+              '0',
+              style: TextStyle(
+                  fontSize: 55,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                style: const TextStyle(color: Colors.black),
+                decoration: InputDecoration(
+                    hintText: 'Please enter the amount in USD',
+                    hintStyle: const TextStyle(color: Colors.black26),
+                    prefixIcon: const Icon(Icons.monetization_on_outlined),
+                    prefixIconColor: Colors.black26,
+                    focusedBorder: border,
+                    filled: true,
+                    fillColor: Colors.white,
+                    enabledBorder: border),
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
